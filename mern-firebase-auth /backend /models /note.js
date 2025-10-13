6be87bdaaae713e73661943125c91e39bf2474c6
Amina Liaquat <aminaliaquat41@gmail.com>
2025-10-13 16:58:45 +0500

    Update note.js

--- mern-firebase-auth /backend /models /note.js
+++ mern-firebase-auth /backend /models /note.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String },
-  userId: { type: String, required: true } // Firebase UID
+  userId: { type: String, required: true } 
 }, { timestamps: true });
 
 export default mongoose.model("Note", noteSchema);
